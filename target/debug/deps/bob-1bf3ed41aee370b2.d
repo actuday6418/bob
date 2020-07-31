5825e5177ad0f396aa5538f27c5587ba47e06923
actuday6418 <actuday6418@gmail.com>
2020-07-31 08:00:21 +0530

    rearranged stuff

--- target/debug/deps/bob-1bf3ed41aee370b2.d
+++
@@ -1,5 +0,0 @@
-/home/actuday/Dev_stuff/GitHub/bob/target/debug/deps/bob-1bf3ed41aee370b2: src/main.rs
-
-/home/actuday/Dev_stuff/GitHub/bob/target/debug/deps/bob-1bf3ed41aee370b2.d: src/main.rs
-
-src/main.rs:
